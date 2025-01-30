3b6cdbad917b8561fd0aa8bb8264e9f5adb071b8
Ignacio Vallarino <ignaciovallarinocibils@gmail.com>
2025-01-30 20:33:47 -0300

    feat: implement memory-based remote configuration and update main app initialization

--- flutter_api_display/lib/main.dart
+++ flutter_api_display/lib/main.dart
@@ -1,68 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_display/remote_config/initialize_remote_config.dart';
+import 'package:flutter_api_display/remote_config/remote_config.dart';
+import 'package:flutter_api_display/utilities/logger.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  AppLogger.info(message: 'Starting Flutter API Display App.');
+  WidgetsFlutterBinding.ensureInitialized();
+
+  AppLogger.info(message: 'Initializing the Remote Config');
+  InitializeRemoteConfig memoryConfig = InitializeMemoryRemoteConfig();
+  RemoteConfig remoteConfig = await memoryConfig.initialize();
+
+  AppLogger.info(message: 'Running the App');
+  runApp(MyApp(
+    remoteConfig: remoteConfig,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final RemoteConfig remoteConfig;
+
+  const MyApp({super.key, required this.remoteConfig});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter API Display',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Center(
+        child: Text(
+            'Hello World we are calling ${remoteConfig.getString(RemoteConfig.apiBaseUrl)}'),
       ),
     );
   }
